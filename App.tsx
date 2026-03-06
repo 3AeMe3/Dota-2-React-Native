1d7e629d3f46e47d05e220318101b250a5df9065
Frank <fm792086@gmail.com>
2026-03-06 12:23:24 -0500

    new project expo/nativewind/

--- App.tsx
+++ App.tsx
@@ -1,14 +1,31 @@
-import { ScreenContent } from 'components/ScreenContent';
-import { StatusBar } from 'expo-status-bar';
-
+import { Image, ImageBackground, Text, TextInput, View } from 'react-native';
 import './global.css';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+import HerosGrid from 'components/heroes-grid';
+
+const image = require('./assets/img/background.webp');
 
 export default function App() {
   return (
     <SafeAreaProvider>
-      <ScreenContent title="Home" path="App.tsx"></ScreenContent>
-      <StatusBar style="auto" />
+      <ImageBackground source={image} resizeMode="cover" className="flex-1 justify-center">
+        <SafeAreaView className="flex-1">
+          <View className="flex-1 items-center  ">
+            <Image
+              source={require('./assets/img/Dota-2-logo.webp')}
+              resizeMode="cover"
+              className="size-12"
+            />
+            <View className="w-full px-10">
+              <TextInput
+                className="border-1  w-full rounded-lg border bg-white  px-4"
+                placeholder="Busca Tu Heroe"
+              />
+            </View>
+            <HerosGrid />
+          </View>
+        </SafeAreaView>
+      </ImageBackground>
     </SafeAreaProvider>
   );
 }
---
+++ components/heroes-grid.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from 'react-native';
+
+export default function HerosGrid() {
+  return (
+    <View>
+      <Text className="text-3xl text-white">HerosGrid</Text>
+    </View>
+  );
+}
