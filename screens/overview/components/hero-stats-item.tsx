9c8099e10cf2a92c09fdbc33616e4bf0a576e394
Frank <fm792086@gmail.com>
2026-03-26 22:57:28 -0500

    hero stats component
    show hero attributes like damage,attack rate, armor, etc

--- screens/overview/components/hero-stats-item.tsx
+++ screens/overview/components/hero-stats-item.tsx
@@ -3,10 +3,10 @@ import { HeroStatsItemProps } from '../types/hero-stats-item.type';
 
 export default function HeroStatsItem({ title, content, footer }: HeroStatsItemProps) {
   return (
-    <View>
-      <Text className="text-gray-400/70">{title || ''}</Text>
-      <Text className="text-white">{content}</Text>
-      <Text className="text-gray-400/70">{footer}</Text>
+    <View className="flex  gap-2">
+      <Text className="text-center text-gray-400/70">{title || ''}</Text>
+      <Text className="text-center font-semibold text-white">{content}</Text>
+      <Text className="text-center text-sm text-gray-400/70">{footer}</Text>
     </View>
   );
 }
--- screens/overview/components/hero-stats-table.tsx
+++ screens/overview/components/hero-stats-table.tsx
@@ -1,55 +1,38 @@
 import { View, Text } from 'react-native';
 import HeroStatsItem from './hero-stats-item';
-import { getAttributeDamage } from '../util/getAttributeDamage';
+import { getAttributeDamage } from '../util/getStatsHero';
 export default function HeroStatsTable({ heroData }) {
   const attributeDamage = getAttributeDamage(heroData);
 
   const minDamage = heroData?.base_attack_min + attributeDamage;
   const maxDamage = heroData?.base_attack_max + attributeDamage;
 
+  const grantsArmor = heroData?.base_agi * 0.167;
+  const mainArmor = (grantsArmor + heroData?.base_armor).toFixed(1);
+  const baseMagicRes = '25%';
+
   return (
-    <View className="min-h-48 flex-col flex-wrap justify-between  rounded-lg bg-[#15151a] p-3">
-      <View className=" flex-row items-center justify-between border border-white  ">
+    <View className="min-h-48 flex-col flex-wrap justify-between  rounded-lg bg-[#15151a] p-5">
+      {/* first row */}
+      <View className=" flex-row items-center justify-between border-b border-gray-200/30 pb-5   ">
         <View className="flex-row gap-5">
-          <HeroStatsItem title="Attack" content={`${minDamage} - ${maxDamage}`} footer="Damage" />
+          <HeroStatsItem title="ATTACK" content={`${minDamage} - ${maxDamage}`} footer="Damage" />
           <HeroStatsItem content={heroData?.attack_rate} footer="Attack Rate" />
         </View>
         <View className="flex-row gap-5">
-          <View>
-            <Text></Text>
-            <Text className="text-white">5.7</Text>
-            <Text className="text-gray-400/70">Armor</Text>
-          </View>
-          <View>
-            <Text className="text-gray-400/70">Defense</Text>
-            <Text className="text-white">30%</Text>
-            <Text className="text-gray-400/70">Magic Res</Text>
-          </View>
+          <HeroStatsItem content={mainArmor} footer="Armor" />
+          <HeroStatsItem title="DEFENSE" content={baseMagicRes} footer="Magic Res" />
         </View>
       </View>
-
-      <View className=" flex-row bg-red-100">
-        <View className="bg-red-100">
-          <View>
-            <Text>Attack</Text>
-            <Text>50 - 54</Text>
-            <Text>Daño</Text>
-          </View>
-          <View>
-            <Text>1.7</Text>
-            <Text>Base Attack</Text>
-          </View>
+      {/* Second row  */}
+      <View className=" flex-row items-center justify-between  border-b border-gray-200/30 py-5  ">
+        <View className="flex-row gap-5">
+          <HeroStatsItem title="MOBILITY" content={heroData?.move_speed} footer="Move Speed" />
+          <HeroStatsItem content={heroData?.attack_type} footer="Range" />
         </View>
-        <View className="bg-red-100">
-          <View>
-            <Text>Attack</Text>
-            <Text>50 - 54</Text>
-            <Text>Daño</Text>
-          </View>
-          <View>
-            <Text>1.7</Text>
-            <Text>Base Attack</Text>
-          </View>
+        <View className="flex-row gap-5">
+          <HeroStatsItem content={heroData?.day_vision} footer="Day" />
+          <HeroStatsItem title="VISION" content={heroData?.night_vision} footer="Night" />
         </View>
       </View>
     </View>
